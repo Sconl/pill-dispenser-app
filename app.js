7e878889af6e062fd63a551a3b2ee1a9e8aaa08e
Sconl Peter <sconl@proton.me>
2023-11-24 02:02:19 +0300

    configuring auth in app.js

--- app.js
+++ app.js
@@ -2,49 +2,64 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const admin = require('firebase-admin');
-const serviceAccount = require('./_assets/christine-840a1-firebase-adminsdk-djrfl-6ef78469e1.json');
+const serviceAccount = require('./_assets/christine-840a1-firebase-adminsdk-djrfl-6ef78469e1.json'); // Update the path
 const http = require('http');
 const socketIo = require('socket.io');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 const io = socketIo(server);
 
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA-4z2Gfzr0psGHBfTYnH-lk_vumhMlkgY",
+  authDomain: "christine-840a1.firebaseapp.com",
+  databaseURL: "https://christine-840a1-default-rtdb.firebaseio.com",
+  projectId: "christine-840a1",
+  storageBucket: "christine-840a1.appspot.com",
+  messagingSenderId: "544139695224",
+  appId: "1:544139695224:web:cc10cfae414c2fe634098d",
+  measurementId: "G-YK3WJEXDNP"
+};
+
 // Initialize Firebase
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://christine-840a1-default-rtdb.firebaseio.com',
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://christine-840a1-default-rtdb.firebaseio.com',
 });
 
 // Configure middleware
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public')); // Serve static files like CSS
 app.set('view engine', 'ejs');
 
 // Import route files
 const settingsRoutes = require('./settings');
 const profileRoutes = require('./profile');
+const { router: authRoutes, isAuthenticated } = require('./routes/auth'); // Import authentication routes
 
 // Use routes
 app.use('/', settingsRoutes);
 app.use('/profile', profileRoutes);
+app.use('/auth', authRoutes); // Use authentication routes
 
 // Real-time updates with Socket.io
 io.on('connection', (socket) => {
-    console.log('A user connected');
+  console.log('A user connected');
 
-    // Example: Listen for real-time updates
-    socket.on('update', (data) => {
-        console.log('Real-time update received:', data);
-        // You can send this data to the client or update the UI accordingly
-    });
+  // Example: Listen for real-time updates
+  socket.on('update', (data) => {
+    console.log('Real-time update received:', data);
+    // You can send this data to the client or update the UI accordingly
+  });
 
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
-    });
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
 });
 
 // Start the server
 server.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
