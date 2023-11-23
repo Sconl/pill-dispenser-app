639a91191a7e5c699085b76812ce8b826745a5f1
Sconl Peter <sconl@proton.me>
2023-11-24 01:07:22 +0300

    Introduce and implement sockets.io

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const admin = require('firebase-admin');
 const serviceAccount = require('./_assets/christine-840a1-firebase-adminsdk-djrfl-6ef78469e1.json');
+const http = require('http');
+const socketIo = require('socket.io');
 
 const app = express();
 const port = 3000;
+const server = http.createServer(app);
+const io = socketIo(server);
 
 // Initialize Firebase
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://christine-840a1-default-rtdb.firebaseio.com', // Replace with your project's database URL
+    databaseURL: 'https://christine-840a1-default-rtdb.firebaseio.com',
 });
 
 // Configure middleware
@@ -19,13 +23,28 @@ app.set('view engine', 'ejs');
 
 // Import route files
 const settingsRoutes = require('./settings');
-const profileRoutes = require('./profile'); // Import the profile routes
+const profileRoutes = require('./profile');
 
 // Use routes
 app.use('/', settingsRoutes);
-app.use('/profile', profileRoutes); // Use the profile routes
+app.use('/profile', profileRoutes);
+
+// Real-time updates with Socket.io
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    // Example: Listen for real-time updates
+    socket.on('update', (data) => {
+        console.log('Real-time update received:', data);
+        // You can send this data to the client or update the UI accordingly
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
 
 // Start the server
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
--- js/settings.js
+++ js/settings.js
@@ -4,27 +4,21 @@ const router = express.Router();
 const admin = require('firebase-admin');
 
 router.get('/settings', async (req, res) => {
-    // Fetch data from Firestore
+    // Fetch user settings data from Firestore
     const userSettings = await getSettingsData('gkjq81JLsV9F8kcAeHpG');
-    const userProfile = await getProfileData('X7Y9LeVsM6IfkXQV0LjI');
 
-    res.render('settings', { userSettings, userProfile });
+    res.render('settings', { userSettings });
 });
 
 router.post('/settings/update', async (req, res) => {
-    // Assuming you have a form with fields for both profile and settings
-    const {
-        name, age, gender, bloodType, allergies,
-        dosage, medicine, set_time, theme
-    } = req.body;
+    // Assuming you have a form with fields for settings
+    const { dosage, medicine, set_time } = req.body;
 
-    // Update the user profile
-    const updatedProfile = { name, age, gender, bloodType, allergies };
-    await updateProfileData('X7Y9LeVsM6IfkXQV0LjI', updatedProfile);
+    // Update the user settings in Firestore
+    await updateSettingsData('gkjq81JLsV9F8kcAeHpG', { dosage, medicine, set_time });
 
-    // Update the user settings
-    const updatedSettings = { dosage, medicine, set_time, theme };
-    await updateSettingsData('gkjq81JLsV9F8kcAeHpG', updatedSettings);
+    // Emit real-time update to all connected clients
+    io.emit('settingsUpdate', { dosage, medicine, set_time });
 
     // Redirect back to the settings page
     res.redirect('/settings');
@@ -45,25 +39,6 @@ async function updateSettingsData(userId, data) {
     const settingsRef = firestore.collection('settings').doc(userId);
 
     await settingsRef.set(data, { merge: true });
-
-    return true;
-}
-
-// Function to get user profile data from Firestore
-async function getProfileData(userId) {
-    const firestore = admin.firestore();
-    const userRef = firestore.collection('users').doc(userId);
-    const snapshot = await userRef.get();
-
-    return snapshot.data();
-}
-
-// Function to update user profile data in Firestore
-async function updateProfileData(userId, data) {
-    const firestore = admin.firestore();
-    const userRef = firestore.collection('users').doc(userId);
-
-    await userRef.set(data, { merge: true });
 
     return true;
 }
