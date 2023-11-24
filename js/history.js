7858ae88d1ba1b1b909099ac12d8e303e5ff4b14
Sconl Peter <sconl@proton.me>
2023-11-24 03:28:50 +0300

    updated the history view

--- js/history.js
+++ js/history.js
@@ -2,17 +2,18 @@ const express = require('express');
 const router = express.Router();
 const admin = require('firebase-admin');
 
-// Placeholder for user history data
-let userHistory = [
-    { date: '2023-01-01', event: 'Visited homepage' },
-    // Add other history entries as needed
-];
-
 router.get('/history', async (req, res) => {
+    // Placeholder for user history data
+    let userHistory = [
+        { time: '12:00 PM', dosage: '1 pill', medicine: 'Medicine A' },
+        { time: '03:30 PM', dosage: '2 pills', medicine: 'Medicine B' },
+        // Add other user history entries as needed
+    ];
+
     // Fetch data from Firestore
     const firestoreData = await getFirestoreData();
 
-    res.render('history', { userHistory, firestoreData });
+    res.render('history', { history: userHistory, firestoreData });
 });
 
 // Function to get data from Firestore
